df343e5918763b59b3b88284fecc79a54ae78048
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2019-10-12 20:45:10 +0200

    Use a SecurityManager and ExitException to handle System.exit-students
    When using System.exit(...), an ExitException will be thrown. This has
    the same effect from the user's point of view (the code exits) as it
    winds up call hierarchy to crash, with the exception it can be caught
    and checked by the test code. If the ExitException is not caught, the
    submission is evaluated as incorrect: students shouldn't use System.exit
    in their code unless asked.

--- src/dodona/junit/JUnitJSON.java
+++ src/dodona/junit/JUnitJSON.java
@@ -1,12 +1,14 @@
 package dodona.junit;
 
-import dodona.feedback.Message;
-import dodona.feedback.AppendMessage;
-import dodona.json.Json;
+import java.util.Optional;
+import java.security.Permission;
+import static java.lang.Thread.currentThread;
 
 import org.junit.runner.JUnitCore;
 
-import static java.lang.Thread.currentThread;
+import dodona.feedback.Message;
+import dodona.feedback.AppendMessage;
+import dodona.json.Json;
 
 public class JUnitJSON {
     public static final String PROPERTY_LANGUAGE = "dodona.language";
@@ -20,9 +22,37 @@ public class JUnitJSON {
             System.exit(1);
         }
 
+        NoExitSecurityManager sm = new NoExitSecurityManager(System.getSecurityManager());
+        System.setSecurityManager(sm);
         JUnitCore core = new JUnitCore();
         core.addListener(new JSONListener());
         core.run(new Class<?>[]{ testSuite });
+        System.setSecurityManager(sm.getPrevious());
+    }
+
+    private static class NoExitSecurityManager extends SecurityManager {
+        private Optional<SecurityManager> previous;
+
+        public NoExitSecurityManager(SecurityManager previous) {
+            this.previous = Optional.ofNullable(previous);
+        }
+
+        @Override public void checkPermission(Permission perm) {
+            previous.ifPresent(sm -> sm.checkPermission(perm));
+        }
+
+        @Override public void checkPermission(Permission perm, Object context) {
+            previous.ifPresent(sm -> sm.checkPermission(perm, context));
+        }
+
+        @Override public void checkExit(int status) {
+            super.checkExit(status);
+            throw new ExitException(status);
+        }
+
+        public SecurityManager getPrevious() {
+            return previous.orElse(null);
+        }
     }
 
 }
