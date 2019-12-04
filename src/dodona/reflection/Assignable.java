5cdf4ad9d0af05c6ebd1a59502e232e7d33f1a44
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2019-12-04 15:42:32 +0100

    Implement more types

--- src/dodona/reflection/Assignable.java
+++ src/dodona/reflection/Assignable.java
@@ -1,6 +1,7 @@
 package dodona.reflection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
@@ -16,6 +17,19 @@ public class Assignable {
 
     public static boolean check(Type a, Type b) {
         return !whynot(a, b).isPresent();
+    }
+
+    private static Optional<String> failComp(String format, Object a, Object b) {
+        return Optional.of(String.format(format, a, b));
+    }
+
+    private static Optional<String> notAssignable(Object a, Object b) {
+        return failComp("not assignable: %s = %s", a, b);
+    }
+
+    private static Optional<String> onlyIfEqual(Type a, Type b) {
+        if(a.equals(b)) return Optional.empty();
+        return failComp("%s is not equal to %s", a, b);
     }
 
     public static Optional<String> whynot(Type a, Type b) {
@@ -48,10 +62,6 @@ public class Assignable {
         return reason;
     }
 
-    private static Optional<String> failComp(String format, Object a, Object b) {
-        return Optional.of(String.format(format, a, b));
-    }
-
     private static Optional<String> specific(GenericArrayType a, GenericArrayType b) {
         return whynot(a.getGenericComponentType(), b.getGenericComponentType());
     }
@@ -61,11 +71,11 @@ public class Assignable {
     }
 
     private static Optional<String> specific(GenericArrayType a, TypeVariable<?> b) {
-        return Optional.of("not yet implemented: GenericArrayType = TypeVariable<?>");
+        return Optional.of("a type variable cannot extend a GenericArrayType");
     }
 
     private static Optional<String> specific(GenericArrayType a, WildcardType b) {
-        return Optional.of("not yet implemented: GenericArrayType = WildcardType");
+        return Optional.of("a wildcard cannot extend a GenericArrayType");
     }
 
     private static Optional<String> specific(GenericArrayType a, Class<?> b) {
@@ -81,23 +91,29 @@ public class Assignable {
         if(base.isPresent()) return base;
         if(a.getActualTypeArguments().length != b.getActualTypeArguments().length) return Optional.of("Huh");
         for(int i = 0; i < a.getActualTypeArguments().length; i++) {
-            if(!a.getActualTypeArguments()[i].equals(b.getActualTypeArguments()[i])) {
-                return failComp(
-                    "un=qual argument types: %s and %s.",
-                    a.getActualTypeArguments()[i],
-                    b.getActualTypeArguments()[i]
-                );
-            }
+            Optional<String> reason = erasableFrom(a.getActualTypeArguments()[i], b.getActualTypeArguments()[i]);
+            if(reason.isPresent()) return reason;
         }
         return Optional.empty();
     }
 
     private static Optional<String> specific(ParameterizedType a, TypeVariable<?> b) {
-        return Optional.of("not yet implemented: ParameterizedType = TypeVariable<?>");
+        for(int i = 0; i < b.getBounds().length; i++) {
+            Optional<String> reason = whynot(a, b.getBounds()[i]);
+            if(reason.isPresent()) return reason;
+        }
+        return Optional.empty();
     }
 
     private static Optional<String> specific(ParameterizedType a, WildcardType b) {
-        return Optional.of("not yet implemented: ParameterizedType = WildcardType");
+        for(int i = 0; i < b.getUpperBounds().length; i++) {
+            Optional<String> reason = whynot(a, b.getUpperBounds()[i]);
+            if(reason.isPresent()) return reason;
+        }
+        if(b.getLowerBounds().length > 0) {
+            return Optional.of("cannot assign a wildcard with lower bounds to a parameterized type");
+        }
+        return Optional.empty();
     }
 
     private static Optional<String> specific(ParameterizedType a, Class<?> b) {
@@ -105,44 +121,62 @@ public class Assignable {
     }
 
     private static Optional<String> specific(TypeVariable<?> a, GenericArrayType b) {
-        return Optional.of("not yet implemented: TypeVariable<?> = GenericArrayType");
+        if(a.getBounds().length == 1 && a.getBounds()[0] == Object.class) return Optional.empty();
+        return Optional.of("cannot assign a generic array type to a type variable with bounds");
     }
 
     private static Optional<String> specific(TypeVariable<?> a, ParameterizedType b) {
-        return Optional.of("not yet implemented: TypeVariable<?> = ParameterizedType");
+        for(Type t : a.getBounds()) {
+            Optional<String> reason = whynot(t, b);
+            if(reason.isPresent()) return reason;
+        }
+        return Optional.empty();
     }
 
     private static Optional<String> specific(TypeVariable<?> a, TypeVariable<?> b) {
-        if(a.equals(b)) return Optional.empty();
-        return failComp("different typevariables: %s and %s.", a, b);
+        for(Type t : a.getBounds()) {
+            if(!Arrays.stream(b.getBounds()).anyMatch(t::equals)) {
+                return failComp("not all bounds of %s were satisfied by %s", a, b);
+            }
+        }
+        return Optional.empty();
     }
 
     private static Optional<String> specific(TypeVariable<?> a, WildcardType b) {
-        return Optional.of("not yet implemented: TypeVariable<?> = WildcardType");
+        for(Type t : a.getBounds()) {
+            if(!Arrays.stream(b.getUpperBounds()).anyMatch(t::equals)) {
+                return failComp("not all bounds of %s were satisfied by %s", a, b);
+            }
+        }
+        return Optional.empty();
     }
 
     private static Optional<String> specific(TypeVariable<?> a, Class<?> b) {
-        return Optional.of("not yet implemented: TypeVariable<?> = Class<?>");
+        for(Type t : a.getBounds()) {
+            Optional<String> reason = whynot(t, b);
+            if(reason.isPresent()) return reason;
+        }
+        return Optional.empty();
     }
 
     private static Optional<String> specific(WildcardType a, GenericArrayType b) {
-        return Optional.of("not yet implemented: WildcardType = GenericArrayType");
+        return Optional.of("cannot write a wildcard type as argument type");
     }
 
     private static Optional<String> specific(WildcardType a, ParameterizedType b) {
-        return Optional.of("not yet implemented: WildcardType = ParameterizedType");
+        return Optional.of("cannot write a wildcard type as argument type");
     }
 
     private static Optional<String> specific(WildcardType a, TypeVariable<?> b) {
-        return Optional.of("not yet implemented: WildcardType = TypeVariable<?>");
+        return Optional.of("cannot write a wildcard type as argument type");
     }
 
     private static Optional<String> specific(WildcardType a, WildcardType b) {
-        return Optional.of("not yet implemented: WildcardType = WildcardType");
+        return Optional.of("cannot write a wildcard type as argument type");
     }
 
     private static Optional<String> specific(WildcardType a, Class<?> b) {
-        return Optional.of("not yet implemented: WildcardType = Class<?>");
+        return Optional.of("cannot write a wildcard type as argument type");
     }
 
     private static Optional<String> specific(Class<?> a, GenericArrayType b) {
@@ -154,11 +188,19 @@ public class Assignable {
     }
 
     private static Optional<String> specific(Class<?> a, TypeVariable<?> b) {
-        return Optional.of("not yet implemented: Class<?> = TypeVariable<?>");
+        for(Type t : b.getBounds()) {
+            Optional<String> reason = whynot(a, t);
+            if(!reason.isPresent()) return Optional.empty();
+        }
+        return failComp("%s as no bound satisfying %s", b, a);
     }
 
     private static Optional<String> specific(Class<?> a, WildcardType b) {
-        return Optional.of("not yet implemented: Class<?> = WildcardType");
+        for(Type t : b.getUpperBounds()) {
+            Optional<String> reason = whynot(a, t);
+            if(!reason.isPresent()) return Optional.empty();
+        }
+        return failComp("%s as no upper bound satisfying %s", b, a);
     }
 
     private static Optional<String> specific(Class<?> a, Class<?> b) {
@@ -167,6 +209,196 @@ public class Assignable {
         } else {
             return failComp("unassignables classes: %s and %s", a, b);
         }
+    }
+
+    private static Optional<String> erasableFrom(Type a, Type b) {
+        Optional<String> reason = failComp("either type is unimplemented: %s = %s", a, b);
+        try{ reason = specificErasableFrom((GenericArrayType)  a, (GenericArrayType)  b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((GenericArrayType)  a, (ParameterizedType) b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((GenericArrayType)  a, (TypeVariable)      b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((GenericArrayType)  a, (WildcardType)      b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((GenericArrayType)  a, (Class)             b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((ParameterizedType) a, (GenericArrayType)  b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((ParameterizedType) a, (ParameterizedType) b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((ParameterizedType) a, (TypeVariable)      b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((ParameterizedType) a, (WildcardType)      b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((ParameterizedType) a, (Class)             b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((TypeVariable)      a, (GenericArrayType)  b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((TypeVariable)      a, (ParameterizedType) b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((TypeVariable)      a, (TypeVariable)      b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((TypeVariable)      a, (WildcardType)      b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((TypeVariable)      a, (Class)             b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((WildcardType)      a, (GenericArrayType)  b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((WildcardType)      a, (ParameterizedType) b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((WildcardType)      a, (TypeVariable)      b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((WildcardType)      a, (WildcardType)      b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((WildcardType)      a, (Class)             b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((Class)             a, (GenericArrayType)  b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((Class)             a, (ParameterizedType) b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((Class)             a, (TypeVariable)      b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((Class)             a, (WildcardType)      b); } catch(ClassCastException e) {}
+        try{ reason = specificErasableFrom((Class)             a, (Class)             b); } catch(ClassCastException e) {}
+        return reason;
+    }
+
+    private static Optional<String> specificErasableFrom(GenericArrayType a, GenericArrayType b) {
+        return onlyIfEqual(a, b);
+    }
+
+    private static Optional<String> specificErasableFrom(GenericArrayType a, ParameterizedType b) {
+        return failComp("cannot erase %s to a %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(GenericArrayType a, TypeVariable<?> b) {
+        return failComp("cannot erase %s to a %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(GenericArrayType a, WildcardType b) {
+        return failComp("cannot erase %s to a %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(GenericArrayType a, Class<?> b) {
+        return failComp("cannot erase %s to a %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(ParameterizedType a, GenericArrayType b) {
+        return failComp("cannot erase %s to a %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(ParameterizedType a, ParameterizedType b) {
+        return onlyIfEqual(a, b);
+    }
+
+    private static Optional<String> specificErasableFrom(ParameterizedType a, TypeVariable<?> b) {
+        for(int i = 0; i < b.getBounds().length; i++) {
+            if(a.equals(b.getBounds()[i])) return Optional.empty();
+        }
+        return failComp("none of the bounds of %s equal to %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(ParameterizedType a, WildcardType b) {
+        for(int i = 0; i < b.getUpperBounds().length; i++) {
+            if(a.equals(b.getUpperBounds()[i])) return Optional.empty();
+        }
+        return failComp("none of the bounds of %s equal to %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(ParameterizedType a, Class<?> b) {
+        return failComp("cannot erase %s to a %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(TypeVariable<?> a, GenericArrayType b) {
+        if(a.getBounds().length == 0) return Optional.empty(); // TODO always object
+        return failComp("cannot erase %s to a %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(TypeVariable<?> a, ParameterizedType b) {
+        if(a.getBounds().length == 0) return Optional.empty(); // TODO always object
+        for(int i = 0; i < a.getBounds().length; i++) {
+            Optional<String> reason = whynot(a.getBounds()[i], b);
+            if(reason.isPresent()) return reason;
+        }
+        return Optional.empty();
+    }
+
+    private static Optional<String> specificErasableFrom(TypeVariable<?> a, TypeVariable<?> b) {
+        for(int i = 0; i < a.getBounds().length; i++) {
+            int j = 0;
+            while(j < b.getBounds().length && !b.getBounds()[j].equals(a.getBounds()[i])) j++;
+            if(j == b.getBounds().length) return failComp("not all bounds of %s were satisfied by %s", a, b);
+        }
+        return Optional.empty();
+    }
+
+    private static Optional<String> specificErasableFrom(TypeVariable<?> a, WildcardType b) {
+        for(int i = 0; i < a.getBounds().length; i++) {
+            int j = 0;
+            while(j < b.getUpperBounds().length && !b.getUpperBounds()[j].equals(a.getBounds()[i])) j++;
+            if(j == b.getUpperBounds().length) return failComp("not all bounds of %s were satisfied by %s", a, b);
+        }
+        return Optional.empty();
+    }
+
+    private static Optional<String> specificErasableFrom(TypeVariable<?> a, Class<?> b) {
+        for(int i = 0; i < a.getBounds().length; i++) {
+            Optional<String> reason = whynot(a.getBounds()[i], b);
+            if(reason.isPresent()) return reason;
+        }
+        return Optional.empty();
+    }
+
+    private static Optional<String> specificErasableFrom(WildcardType a, GenericArrayType b) {
+        if(a.getLowerBounds().length != 0) return Optional.of("a generic array type could never satisfy a lower bound");
+        if(a.getUpperBounds().length == 1 && a.getUpperBounds()[0] == Object.class) return Optional.empty();
+        return Optional.of("a generic array type could never satisfy an upper bound");
+    }
+
+    private static Optional<String> specificErasableFrom(WildcardType a, ParameterizedType b) {
+        for(int i = 0; i < a.getLowerBounds().length; i++) {
+            Optional<String> reason = whynot(b, a.getLowerBounds()[i]);
+            if(reason.isPresent()) return reason;
+        }
+        for(int i = 0; i < a.getUpperBounds().length; i++) {
+            Optional<String> reason = whynot(a.getUpperBounds()[i], b);
+            if(reason.isPresent()) return reason;
+        }
+        return Optional.empty();
+    }
+
+    private static Optional<String> specificErasableFrom(WildcardType a, TypeVariable<?> b) {
+        if(a.getLowerBounds().length != 0) return Optional.of("a type variable could never satisfy a lower bound");
+        for(int i = 0; i < a.getUpperBounds().length; i++) {
+            int j = 0;
+            while(j < b.getBounds().length && !b.getBounds()[j].equals(a.getUpperBounds()[i])) j++;
+            if(j == b.getBounds().length) return failComp("not all bounds of %s were satisfied by %s", a, b);
+        }
+        return Optional.empty();
+    }
+
+    private static Optional<String> specificErasableFrom(WildcardType a, WildcardType b) {
+        for(int i = 0; i < a.getUpperBounds().length; i++) {
+            int j = 0;
+            while(j < b.getUpperBounds().length && !b.getUpperBounds()[j].equals(a.getUpperBounds()[i])) j++;
+            if(j == b.getUpperBounds().length) return failComp("not all bounds of %s were satisfied by %s", a, b);
+        }
+        for(int i = 0; i < b.getLowerBounds().length; i++) {
+            int j = 0;
+            while(j < a.getLowerBounds().length && !a.getLowerBounds()[j].equals(b.getLowerBounds()[i])) j++;
+            if(j == a.getLowerBounds().length) return failComp("not all bounds of %s were satisfied by %s", b, a);
+        }
+        return Optional.empty();
+    }
+
+    private static Optional<String> specificErasableFrom(WildcardType a, Class<?> b) {
+        for(int i = 0; i < a.getLowerBounds().length; i++) {
+            Optional<String> reason = whynot(b, a.getLowerBounds()[i]);
+            if(reason.isPresent()) return reason;
+        }
+        for(int i = 0; i < a.getUpperBounds().length; i++) {
+            Optional<String> reason = whynot(a.getUpperBounds()[i], b);
+            if(reason.isPresent()) return reason;
+        }
+        return Optional.empty();
+    }
+
+    private static Optional<String> specificErasableFrom(Class<?> a, GenericArrayType b) {
+        return failComp("cannot erase %s to a %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(Class<?> a, ParameterizedType b) {
+        return failComp("cannot erase %s to a %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(Class<?> a, TypeVariable<?> b) {
+        return failComp("cannot erase %s to a %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(Class<?> a, WildcardType b) {
+        return failComp("cannot erase %s to a %s", b, a);
+    }
+
+    private static Optional<String> specificErasableFrom(Class<?> a, Class<?> b) {
+        return onlyIfEqual(a, b);
     }
 
 }
