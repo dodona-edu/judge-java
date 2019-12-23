953b224ca7955d9531f53058744dfd113c711c9c
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2019-12-23 17:06:27 +0100

    Test a wrongly typed stubbed method

---
+++ integration-tests/stubber-wrong-type/evaluation/StubberInterface.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public interface StubberInterface {
+
+    void intArray(int[] arg);
+    void intArrayList(ArrayList<Integer> arg);
+
+}
---
+++ integration-tests/stubber-wrong-type/submission.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Stubber {
+
+    public void intArray(double[] arg) {}
+    public void intArrayList(ArrayList<Double> arg) {}
+
+}
