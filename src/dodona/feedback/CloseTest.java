313de7c3f8a21918f326c80c93064e1c448ba582
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2020-01-30 09:22:44 +0100

    Truncate output exceeding a configurable cutoff

--- src/dodona/feedback/CloseTest.java
+++ src/dodona/feedback/CloseTest.java
@@ -1,14 +1,27 @@
 package dodona.feedback;
 
+import static dodona.junit.JUnitJSON.PROPERTY_OUTPUT_CUTOFF;
+
 public class CloseTest extends AcceptedClose {
+
+    // must be passed to the judge
+    private static final int outputCutoff = Integer.getInteger(PROPERTY_OUTPUT_CUTOFF);
 
     private String generated;
     private StatusPair status;
 
     public CloseTest(String generated, StatusPair status, boolean accepted) {
         super("close-test", accepted);
-        this.generated = generated;
+        if(generated.length() > outputCutoff) {
+            this.generated = generated.substring(0, outputCutoff) + "...";
+        } else {
+            this.generated = generated;
+        }
         this.status = status;
+    }
+
+    public boolean wasTruncated() {
+        return this.generated.length() > outputCutoff;
     }
 
 }
